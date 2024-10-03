019f3173ce50d3b65e475824ee1520a3fe27b5a0
SY752 <shaistay752@gmail.com>
2024-10-03 11:03:09 +0530

    yasmeen

---
+++ core-java/Method.java
@@ -0,0 +1,16 @@
+public class Method{
+	
+	public static void main(String[] args){
+    	
+    	product(34,6);
+    	product(2000,4);
+
+		
+}
+	public static void product(int num1 , int num2){
+		int multiplication= num1 * num2;
+	  	
+		System.out.println(multiplication);
+
+	}
+}		
