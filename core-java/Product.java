c53bc9157e242520d85a3ebba2b31e73ecbc131c
SY752 <shaistay752@gmail.com>
2024-10-03 09:58:30 +0530

    shaista

---
+++ core-java/Product.java
@@ -0,0 +1,9 @@
+public class Product{
+	
+	public static void main(String[] args){
+    	usn(45764);
+}
+   public static void usn(int regNumber){
+   		System.out.println("your usn is:" + regNumber);
+	}
+}
